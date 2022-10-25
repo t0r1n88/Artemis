cd3c5709b7182634f7d05a402a2ec7dbd87e4dfa
Oleg Budaev <t0r1n88@mail.ru>
2022-10-25 13:42:55 +0800

    Обновил проверку наличия записи в реестре+

--- forest_merge_reestr.py
+++ forest_merge_reestr.py
@@ -1,17 +1,15 @@
 """
-Проверка наличия данных из ведомости  в реестре УПП с отображением
+Проверка наличия данных из ведомости лесов в реестре УПП с отображением
 """
 import pandas as pd
 import openpyxl
 import warnings
-
 warnings.filterwarnings('ignore', category=UserWarning, module='openpyxl')
 from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.styles import Alignment
 import time
 
 pd.options.mode.chained_assignment = None  # default='warn'
-
 
 def convert_columns_to_str(df, number_columns):
     """
@@ -31,27 +29,24 @@ file_statement_presense = 'data/Сравнение УПП с другими ве
 
 # Считываем из файлов только те колонки по которым будет вестись сравнение
 first_df = pd.read_excel(file_reestr_presense,
-                         skiprows=8, usecols=[0, 1, 2, 3, 4], keep_default_na=False)
+                         skiprows=8, usecols=[0,1,2,3,4], keep_default_na=False)
 second_df = pd.read_excel(file_statement_presense, keep_default_na=False)
-
-first_df.head()
-
-second_df.head()
 
 # Приводим датафреймы к строковому виду
 first_df = first_df.astype(str)
 second_df = second_df.astype(str)
 
 # Очищаем от
-first_df.replace(r'^\s*$', 'Отсутствует', regex=True, inplace=True)
-second_df.replace(r'^\s*$', 'Отсутствует', regex=True, inplace=True)
+first_df.replace(r'^\s*$', 'Отсутствует', regex=True,inplace=True)
+second_df.replace(r'^\s*$', 'Отсутствует', regex=True,inplace=True)
+
+
 
 # так как мы заранее знаем сколько и какие колонки у нас есть то просто создаем список
-params_columns = [0, 1, 2, 3, 4]
+params_columns = [0,1,2,3,4]
 # Конвертируем нужные нам колонки в str
 convert_columns_to_str(first_df, params_columns)
 convert_columns_to_str(second_df, params_columns)
-
 
 # Создаем в каждом датафрейме колонку с айди путем склеивания всех нужных колонок в одну строку
 first_df['ID'] = first_df.iloc[:, params_columns].sum(axis=1)
@@ -86,9 +81,9 @@ out_df['Присутствие в реестре УПП'] = out_df['Присут
 current_time = time.strftime('%H_%M_%S %d.%m.%Y')
 # Сохраняем отчет
 # Для того чтобы увеличить ширину колонок для удобства чтения используем openpyxl
-wb = openpyxl.Workbook()  # Создаем объект
+wb = openpyxl.Workbook() # Создаем объект
 # Записываем результаты
-for row in dataframe_to_rows(out_df, index=False, header=True):
+for row in dataframe_to_rows(out_df,index=False,header=True):
     wb['Sheet'].append(row)
 
 # Форматирование итоговой таблицы
@@ -105,5 +100,13 @@ wb['Sheet']['F1'].alignment = Alignment(wrap_text=True)
 wb['Sheet']['G1'].alignment = Alignment(wrap_text=True)
 wb['Sheet']['H1'].alignment = Alignment(wrap_text=True)
 
-wb.save(
-    f'{path_to_end_folder}/Сравнение УПП с другими ведомостями на наличие участков или их отсутствие от  {current_time}.xlsx')
+wb.save(f'{path_to_end_folder}/Сравнение УПП с другими ведомостями на наличие участков или их отсутствие от  {current_time}.xlsx')
+
+
+
+
+
+
+
+
+
